de975168628f8de805f8312c3e35334dcc718822
GoPenguinGo <75738992+GoPenguinGo@users.noreply.github.com>
2022-04-05 19:57:33 +0200

    merge

--- main.py
+++ main.py
@@ -14,7 +14,6 @@ import concurrent.futures
 # for k in range(Mpaths):
 def simulate(k):
     s = time.time()
-
     time_s = time.time()
     dZt = dt**0.5 * np.random.randn(int(Nt - 1))
     (
@@ -79,7 +78,6 @@ def simulate(k):
 
 ks = [k for k in range(10)]
 
-
 def main():
     with concurrent.futures.ProcessPoolExecutor(max_workers=3) as executor:
         for k, result in zip(ks, executor.map(simulate, ks)):
@@ -91,62 +89,68 @@ if __name__ == "__main__":
     main()
     print(time.time() - time_s)
 
-    # erp_S = mu_S - r
-    # erp_hat_S = mu_hat_S - r
-    # erp_S_s = mu_S_s - r
+    erp_S = mu_S - r
+    erp_hat_S = mu_hat_S - r
+    erp_S_s = mu_S_s - np.reshape(r, (Nt, 1))
 
-    # time_assignment = time.time()
-    # Z_matrix[k, :] = Zt
-    # dR_matrix[k, :] = dR
-    # # EtMAT[k, :] = np.transpose(Et)
-    # # VtMAT[k, :] = np.transpose(Vt)
-    # Delta_matrix[k, :, :] = BIGDELTA
-    # r_matrix[k, :] = r
-    # theta_matrix[k, :] = theta
-    # mu_S_matrix[k, :] = mu_S
-    # mu_S_s_matrix[k, :, :] = mu_S_s
-    # mu_hat_S_matrix[k, :] = mu_hat_S
-    # erp_S_matrix[k, :] = erp_S
-    # erp_S_s_matrix[k, :, :] = erp_S_s
-    # erp_hat_S_matrix[k, :] = erp_hat_S
-    # port_matrix[k, :, :] = BIGPORT
-
-    # print(time.time() - time_assignment)
-    # # assert False
-    # corrMuSmuHat[k] = np.corrcoef(mu_hat_S, mu_S)[0, 1]
-    # F_Matrix[k, :, :] = np.mean(BIGF, axis=0)
-    # print(time.time() - s)
+    Z_matrix[k, :] = Zt
+    dR_matrix[k, :] = dR
+    # EtMAT[k, :] = np.transpose(Et)
+    # VtMAT[k, :] = np.transpose(Vt)
+    Delta_matrix[k, :, :] = BIGDELTA
+    r_matrix[k, :] = r
+    theta_matrix[k, :] = theta
+    mu_S_matrix[k, :] = mu_S
+    mu_S_s_matrix[k, :, :] = mu_S_s
+    mu_hat_S_matrix[k, :] = mu_hat_S
+    erp_S_matrix[k, :] = erp_S
+    erp_S_s_matrix[k, :, :] = erp_S_s
+    erp_hat_S_matrix[k, :] = erp_hat_S
+    port_matrix[k, :, :] = BIGPORT
+    f_matrix[k, :, :] = BIGF
+    fcondi_matrix[k, :] = BIGFCONDI
 
 
-# for k in range(Mpaths):
-#     for l in range(Nsamples):
-#         a = int(l * stepcorr)
-#         b = int((l + 1) * stepcorr)
-#         corrZMUs_t[k, l] = np.corrcoef(Z_matrix[k, a:b], mu_S_s_matrix[k, Nt-1, a:b])[0, 1]
-#         corrZport[k, l] = np.corrcoef(Z_matrix[k, a:b], port_matrix[k, Nt-1, a:b])[0, 1]
-#         corrMU_sMUs_t[k, l] = np.corrcoef(mu_S_matrix[k, a:b], mu_S_s_matrix[k, Nt-1, a:b])[0, 1]
-#         # muCst[k, l] = np.mean(muC_s_t[a:b])
-#         # logmuCst[k, l] = np.mean(muC_s_t[a:b]) - 0.5 * sum((sigmaC_s_t[a:b]) ** 2)
-#         # sigCst[k, l] = np.mean(sigmaC_s_t[a:b])
-#         # stdCst[k, l] = np.mean(abs(sigmaC_s_t[a:b]))
+    if k % 10 == 0:
+        print(k)
+        print(time.time() - s)
 
-# MaxAge = 100
-# MaxAgeN = int(MaxAge / Tsample)
-# tperiod = range(Tsample, 100 + Tsample, Tsample)
-# meanZport = np.mean(corrZport, axis=0)
-# meanZmus_t = np.mean(corrZMUs_t, axis=0)
 
-# # Compute the mean values from the simulation
-# meanMus = np.mean(corrMU_sMUs_t, axis=0)
-# # meanMuCst = np.mean(muCst, axis=0)
-# # meanSCst = np.mean(sigCst, axis=0)
-# # meanStdCst = np.mean(stdCst, axis=0)
-# # meanLogMuCst = np.mean(logmuCst, axis=0)
 
-# # Figures
-# # Figure 1 in the paper
-# fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(4, 10))
-# ax1.plot(tperiod, meanMus[:MaxAgeN])
-# ax2.plot(tperiod, meanZmus_t[:MaxAgeN])
+for k in range(Mpaths):
+    corrMuSmuHat[k] = np.corrcoef(mu_hat_S_matrix[k], mu_S_matrix[k])[0, 1]
+    for l in range(Nsamples):
+        # a = int(l * stepcorr)
+        # b = int((l + 1) * stepcorr)
+        # corrZMUs_t[k, l] = np.corrcoef(Z_matrix[k, a:b], mu_S_s_matrix[k, a:b, Nc-1])[0, 1]
+        # corrZport[k, l] = np.corrcoef(Z_matrix[k, a:b], port_matrix[k, a:b, Nc-1])[0, 1]
+        # corrMU_sMUs_t[k, l] = np.corrcoef(mu_S_matrix[k, a:b], mu_S_s_matrix[k, a:b, Nc-1])[0, 1]
+        corrZMUs_t[k, l] = np.corrcoef(Z_matrix[k, ], erp_S_s_matrix[k, :, -l])[0, 1]
+        corrZport[k, l] = np.corrcoef(Z_matrix[k, ], port_matrix[k, :, -l])[0, 1]
+        corrMU_sMUs_t[k, l] = np.corrcoef(erp_S_matrix[k, ], erp_S_s_matrix[k, :, -l])[0, 1]
+        # muCst[k, l] = np.mean(muC_s_t[a:b])
+        # logmuCst[k, l] = np.mean(muC_s_t[a:b]) - 0.5 * sum((sigmaC_s_t[a:b]) ** 2)
+        # sigCst[k, l] = np.mean(sigmaC_s_t[a:b])
+        # stdCst[k, l] = np.mean(abs(sigmaC_s_t[a:b]))
 
-# # Figure 2 in the paper
+MaxAge = 100
+MaxAgeN = int(MaxAge / Tsample)
+tperiod = range(Tsample, 100 + Tsample, Tsample)
+meanZport = np.mean(corrZport, axis=0)
+meanZmus_t = np.mean(corrZMUs_t, axis=0)
+
+# Compute the mean values from the simulation
+meanMus = np.mean(corrMU_sMUs_t, axis=0)
+# meanMuCst = np.mean(muCst, axis=0)
+# meanSCst = np.mean(sigCst, axis=0)
+# meanStdCst = np.mean(stdCst, axis=0)
+# meanLogMuCst = np.mean(logmuCst, axis=0)
+
+# Figures
+# Figure 1 in the paper
+fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(4, 10))
+ax1.plot(tperiod, meanMus[:MaxAgeN])
+ax2.plot(tperiod, meanZmus_t[:MaxAgeN])
+
+# Figure 2 in the paper
+
--- src/param.py
+++ src/param.py
@@ -11,37 +11,50 @@ sigma_S = (
 w = 0.92  # Fraction of total output paid out as endowment
 
 # Some pre-calculations
-D = (rho + nu) * (rho + nu - 4 * nu**2)
-beta = (rho + nu - D**0.5) / (2 * nu)
+D = (rho + nu) * (rho + nu - 4 * nu ** 2)
+beta = (rho + nu - D ** 0.5) / (2 * nu)
 
 # Setting prior variance
 T_hat = 20  # Pre-trading period
 dt = 1 / 12  # time incremental
 Npre = int(T_hat / dt)
-Vhat = (sigma_Y**2) / T_hat  # prior variance
+Vhat = (sigma_Y ** 2) / T_hat  # prior variance
 T_cohort = 500  # time horizon to keep track of cohorts
 Nt = int(T_cohort / dt)  # number of periods
 Nc = int(T_cohort / dt)  # number of cohorts
 
 time_tolerance = 5
 
-# Initializing some variables
 Mpaths = 100
+
+# for graphs:
 Tsample = int(T_cohort / 100)
-Nsamples = 100
+Nsamples = 500
 stepcorr = int(Tsample / dt)
 corrZport = np.zeros((Mpaths, Nsamples))
 corrZMUs_t = np.zeros((Mpaths, Nsamples))
 corrMU_sMUs_t = np.zeros((Mpaths, Nsamples))
 corrMuSmuHat = np.zeros((Mpaths, 1))
-F_Matrix = np.zeros((Mpaths, Nt, Nc))
+
+mu_C_s_t = np.zeros((Mpaths, Nsamples))
+log_mu_C_s_t = np.zeros((Mpaths, Nsamples))
+sigma_C_s_t = np.zeros((Mpaths, Nsamples))
+std_C_s_t = np.zeros((Mpaths, Nsamples))
+
+####################################################################################
+# Store the values from the main loop
+Z_matrix = np.zeros((Mpaths, Nt))
 mu_C_matrix = np.zeros((Mpaths, Nt))
 sigma_C_matrix = np.zeros((Mpaths, Nt))
-Delta_matrix = np.zeros((Mpaths, Nt, Nc))
+delta_matrix = np.zeros((Mpaths, Nt, Nc))
 r_matrix = np.zeros((Mpaths, Nt))
+f_matrix = np.zeros((Mpaths, Nt, Nc))
 theta_matrix = np.zeros((Mpaths, Nt))
 port_matrix = np.zeros((Mpaths, Nt, Nc))
-Z_matrix = np.zeros((Mpaths, Nt))
+fcondi_matrix = np.zeros((Mpaths, Nt))
+popu_matrix = np.zeros((Mpaths, Nt))
+delta_condi_matrix = np.zeros((Mpaths, Nt))
+
 
 # Expected returns
 mu_S_matrix = np.zeros((Mpaths, Nt))  # Expected returns under the true measure
@@ -53,14 +66,19 @@ mu_hat_S_matrix = np.zeros(
 )  # Simple average of expected returns, or consensus belief
 
 # Equity risk premium
-erp_S_matrix = np.zeros((Mpaths, Nt))
-erp_S_s_matrix = np.zeros((Mpaths, Nt, Nc))
-erp_hat_S_matrix = np.zeros((Mpaths, Nt))
+erp_S_matrix = np.zeros(
+    (Mpaths, Nt)
+)
+
+erp_S_s_matrix = np.zeros(
+    (Mpaths, Nt, Nc)
+)
+erp_hat_S_matrix = np.zeros(
+    (Mpaths, Nt)
+)
+
 
 Et_matrix = np.zeros((Mpaths, Nt))
 Vt_matrix = np.zeros((Mpaths, Nt))
 dR_matrix = np.zeros((Mpaths, Nt))
-mu_C_s_t = np.zeros((Mpaths, Nsamples))
-log_mu_C_s_t = np.zeros((Mpaths, Nsamples))
-sigma_C_s_t = np.zeros((Mpaths, Nsamples))
-std_C_s_t = np.zeros((Mpaths, Nsamples))
+
