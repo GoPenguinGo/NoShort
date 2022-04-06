41ffcee9c6ecf055020779123c06822421e8e070
GoPenguinGo <75738992+GoPenguinGo@users.noreply.github.com>
2022-04-06 21:32:26 +0200

    add functions, add drop mode to the functions
    Complete market version for the two cohort functions:
    (1) simulate_cohorts_complete_market
    (2) build_cohorts_complete_market
    to generate benchmark results to learn the impact of having the short-sale constraint
    Add a different mode to the functions, implying a different setting that if a cohort stop investing, they are out of the stock market forever.

--- main.py
+++ main.py
@@ -24,7 +24,19 @@ for k in range(Mpaths):
         Zt,
         tau,
         MaxThetaDelta_s_t,
-    ) = build_cohorts(dZt, Nc, dt, rho, nu, Vhat, mu_Y, sigma_Y, beta, T_hat)
+        invest_tracker_keep,
+    ) = build_cohorts(dZt, Nc, dt, rho, nu, Vhat, mu_Y, sigma_Y, beta, T_hat, mode1)
+
+    (
+        IntVec_drop,
+        Xt_drop,
+        Delta_s_t_drop,
+        Yt_drop,
+        Zt_drop,
+        tau_drop,
+        MaxThetaDelta_s_t_drop,
+        invest_tracker,
+    ) = build_cohorts(dZt, Nc, dt, rho, nu, Vhat, mu_Y, sigma_Y, beta, T_hat, mode2)
     if time.time() - time_s > time_tolerance:
         print(f"It takes more than {time_tolerance}s to build up the cohorts")
 
@@ -70,6 +82,48 @@ for k in range(Mpaths):
         beta,
         T_hat,
         Npre,
+        mode1,
+        invest_tracker_keep,
+    )
+
+    (
+        Xt2_drop,
+        part1_drop,
+        mu_S_drop,
+        mu_S_s_drop,
+        mu_hat_S_drop,
+        r_drop,
+        theta_drop,
+        BIGF_drop,
+        BIGDELTA_drop,
+        BIGMAX_drop,
+        BIGPORT_drop,
+        BIGPOPU_drop,
+        BIGFCONDI_drop,
+        BIGDELTABARCONDI_drop,
+        dR_drop,
+
+    ) = simulate_cohorts(
+        biasvec,
+        dZt,
+        Nt,
+        Nc,
+        tau,
+        IntVec,
+        Delta_s_t,
+        MaxThetaDelta_s_t,
+        dt,
+        rho,
+        nu,
+        Vhat,
+        mu_Y,
+        sigma_Y,
+        sigma_S,
+        beta,
+        T_hat,
+        Npre,
+        mode2,
+        invest_tracker,
     )
 
     erp_S = mu_S - r
@@ -95,7 +149,36 @@ for k in range(Mpaths):
     fcondi_matrix[k, :] = BIGFCONDI
     popu_matrix[k, :] = BIGPOPU
     delta_condi_matrix[k, :] = BIGDELTABARCONDI
-    
+
+
+
+
+    ####################### for the drop case:
+
+    erp_S_drop = mu_S_drop - r_drop
+    erp_hat_S_drop = mu_hat_S_drop - r_drop
+    erp_S_s_drop = mu_S_s_drop - np.reshape(r_drop, (Nt, 1))
+
+    # Z_matrix[k, :] = Zt
+    dR_matrix_drop[k, :] = dR_drop
+    # EtMAT[k, :] = np.transpose(Et)
+    # VtMAT[k, :] = np.transpose(Vt)
+    delta_matrix_drop[k, :, :] = BIGDELTA_drop
+    r_matrix_drop[k, :] = r_drop
+    theta_matrix_drop[k, :] = theta_drop
+    mu_S_matrix_drop[k, :] = mu_S_drop
+    mu_S_s_matrix_drop[k, :, :] = mu_S_s_drop
+    mu_hat_S_matrix_drop[k, :] = mu_hat_S_drop
+    erp_S_matrix_drop[k, :] = erp_S_drop
+    erp_S_s_matrix_drop[k, :, :] = erp_S_s_drop
+    erp_hat_S_matrix_drop[k, :] = erp_hat_S_drop
+    port_matrix_drop[k, :, :] = BIGPORT_drop
+
+    f_matrix_drop[k, :, :] = BIGF_drop
+    fcondi_matrix_drop[k, :] = BIGFCONDI_drop
+    popu_matrix_drop[k, :] = BIGPOPU_drop
+    delta_condi_matrix_drop[k, :] = BIGDELTABARCONDI_drop
+
     print(time.time() - s)
 
 change_popu_matrix = BIGPOPU[1:]/BIGPOPU[:-1]
@@ -105,7 +188,7 @@ change_popu_matrix = BIGPOPU[1:]/BIGPOPU[:-1]
 
 # keep only the last 100 years data
 
-# (1) Zt and Participation Rate from one random path
+# (1.1) Zt and Participation Rate from one random path
 t = np.arange(0, T_cohort, dt)  # has the same length as Nc
 random_paths = 1  # 1 can be any random number from 0 to Mpaths
 y1 = Z_matrix[random_paths, :]
@@ -127,10 +210,10 @@ fig.suptitle('Zt and Participation Rate')
 fig.tight_layout()  # otherwise the right y-label is slightly clipped
 plt.show()
 
-# (2) dZt and Log Change in Participation Rate
+# (1.2) dZt and Log Change in Participation Rate
 sample = np.arange(Nc - Nkeep - 1, Nc - 1, 20)
 x1 = Z_matrix[:, sample] - Z_matrix[:, sample - 1]
-y1 = np.log(popu_matrix[:, sample + 1] / popu_matrix[:, sample])
+y1 = np.log(popu_matrix[:, sample + 1] / popu_matrix[:, sample])  # participation rate is lagged because of simulate_cohorts construction
 plt.scatter(x1, y1, marker = '.', c = 'b', alpha = 0.8)
 plt.xlabel('dZt')
 plt.ylabel('log changes in participation rate')
@@ -138,6 +221,44 @@ plt.title('dZt and Log Change in Participation Rate')
 plt.show()
 
 
+
+############################## for the drop case:
+
+# (1.1) Zt and Participation Rate from one random path
+t = np.arange(0, T_cohort, dt)  # has the same length as Nc
+random_paths = 1  # 1 can be any random number from 0 to Mpaths
+y1 = Z_matrix[random_paths, :]
+y2 = popu_matrix_drop[random_paths, :]
+fig, ax1 = plt.subplots()
+color1 = 'r'
+ax1.set_xlabel('time in simulation, one random path')
+ax1.set_ylabel('Zt', color = color1)
+ax1.plot(t, y1, color = color1, linewidth = 0.8)
+ax1.tick_params(axis='y', labelcolor = color1)
+
+ax2 = ax1.twinx()
+color2 = 'b'
+ax2.set_ylabel('Population holding stocks, drop', color = color2)
+ax2.plot(t, y2, color = color2, linewidth = 0.8)
+ax2.tick_params(axis='y', labelcolor = color2)
+
+fig.suptitle('Zt and Participation Rate, if Drop')
+fig.tight_layout()  # otherwise the right y-label is slightly clipped
+plt.show()
+
+# (1.2) dZt and Log Change in Participation Rate
+sample = np.arange(Nc - Nkeep - 1, Nc - 1, 20)
+x1 = Z_matrix[:, sample] - Z_matrix[:, sample - 1]
+y1 = np.log(popu_matrix_drop[:, sample + 1] / popu_matrix_drop[:, sample])  # participation rate is lagged because of simulate_cohorts construction
+plt.scatter(x1, y1, marker = '.', c = 'b', alpha = 0.8)
+plt.xlabel('dZt')
+plt.ylabel('log changes in participation rate')
+plt.title('dZt and Log Change in Participation Rate')
+plt.show()
+
+###################################################
+
+# (2.1)
 for k in range(Mpaths):
     corrMuSmuHat[k] = np.corrcoef(mu_hat_S_matrix[k], mu_S_matrix[k])[0, 1]
     for l in range(Nsamples):
--- src/cohort_simulator.py
+++ src/cohort_simulator.py
@@ -6,24 +6,26 @@ from tqdm import tqdm
 
 
 def simulate_cohorts(
-    biasvec: np.ndarray,
-    dZt: np.ndarray,
-    Nt: int,
-    Nc: int,
-    tau: np.ndarray,
-    IntVec: np.ndarray,
-    Delta_s_t: np.ndarray,
-    MaxThetaDelta_s_t: np.ndarray,
-    dt: float,
-    rho: float,
-    nu: float,
-    Vhat: float,
-    mu_Y: float,
-    sigma_Y: float,
-    sigma_S: float,
-    beta: float,
-    T_hat: float,
-    Npre: float,
+        biasvec: np.ndarray,
+        dZt: np.ndarray,
+        Nt: int,
+        Nc: int,
+        tau: np.ndarray,
+        IntVec: np.ndarray,
+        Delta_s_t: np.ndarray,
+        MaxThetaDelta_s_t: np.ndarray,
+        dt: float,
+        rho: float,
+        nu: float,
+        Vhat: float,
+        mu_Y: float,
+        sigma_Y: float,
+        sigma_S: float,
+        beta: float,
+        T_hat: float,
+        Npre: float,
+        mode: str,
+        invest_tracker: np.ndarray
 ) -> Tuple[
     np.ndarray,
     np.ndarray,
@@ -39,7 +41,7 @@ def simulate_cohorts(
     np.ndarray,
 ]:
     """"" Simulate the economy forward
-    
+
     Args:
         biasvec (np.ndarray): pre-trading period shocks to form beliefs of the initial cohort, shape(Npre,)
         dZt (np.ndarray): random shocks, shape (Nt)
@@ -59,7 +61,9 @@ def simulate_cohorts(
         beta (float): as in eq(18), consumption share of the newborn cohort
         T_hat (float): pre-trading years
         Npre (float): pre-trading number of obs
-    
+        mode (str)
+        invest_tracker (np.ndarry): shape(Nt)
+
     Returns:
         Xt2 (np.ndarray): xi_t * Yt, shape(Nt, )
         part1 (np.ndarray): Consumption of each cohort, eq(16), where eta_s_t / eta_s_s follows eq(11), shape(Nc, )
@@ -78,7 +82,7 @@ def simulate_cohorts(
         BIGFCONDI (np.ndarray): aggregate consumption share over time conditional on invest in stocks, shape(Nt, )
         BIGDELTABARCONDI (np.ndarray): aggregate consumption weighted bias over time conditional on invest in stocks, shape(Nt, )
         dR (np.ndarray): change of stock returns over time, shape(Nt, )
-        
+
     """ ""
     # Initializing variables
     Xt2 = np.ones(Nt)
@@ -115,9 +119,9 @@ def simulate_cohorts(
     theta = np.zeros(Nt)  # market price of risk
 
     for i in tqdm(range(0, Nt)):
-
+        # todo: think about the time sequence of how things happen
         Part = IntVec * np.exp(
-            -(0.5 * MaxThetaDelta_s_t**2) * dt + MaxThetaDelta_s_t * dZt[i]
+            -(0.5 * MaxThetaDelta_s_t ** 2) * dt + MaxThetaDelta_s_t * dZt[i]
         )
 
         sumPart = np.sum(Part)
@@ -126,26 +130,43 @@ def simulate_cohorts(
 
         # find theta
         A = -np.max(Delta_s_t)
-        theta_t = bisection(
-            solve_theta, A, 10, f, Delta_s_t, sigma_Y
-        )
-        MaxThetaDelta_s_t = np.maximum(-theta_t, Delta_s_t)
-        invest = Delta_s_t >= -theta_t
-        invest_f = invest * f
-        popuCondi = np.sum(cohort_size * invest)
-        DeltabarCondi = np.sum(Delta_s_t * invest_f)
-        fCondi = np.sum(invest_f)
+        if mode == 'drop':
+            possible_f = f * invest_tracker
+            possible_delta_st = Delta_s_t * invest_tracker
+            theta_t = bisection(
+                solve_theta, A, 10, possible_f, possible_delta_st, sigma_Y
+            )
+            MaxThetaDelta_s_t = np.maximum(-theta_t, Delta_s_t)
+            invest = Delta_s_t >= -theta_t
+            invest_tracker = invest_tracker * invest
+            invest_f = invest_tracker * f
+            popuCondi = np.sum(cohort_size * invest_tracker)
+            DeltabarCondi = np.sum(Delta_s_t * invest_f)
+            fCondi = np.sum(invest_f)
+            invest_tracker = invest_tracker[1:]
+            invest_tracker = np.append(invest_tracker, 1)
+
+        else:
+            theta_t = bisection(
+                solve_theta, A, 10, f, Delta_s_t, sigma_Y
+            )
+            MaxThetaDelta_s_t = np.maximum(-theta_t, Delta_s_t)
+            invest = Delta_s_t >= -theta_t
+            invest_f = invest * f
+            popuCondi = np.sum(cohort_size * invest)
+            DeltabarCondi = np.sum(Delta_s_t * invest_f)
+            fCondi = np.sum(invest_f)
 
         r_t = (
-            rho
-            + mu_Y
-            + nu * (1 - beta)
-            - (sigma_Y**2 - sigma_Y * DeltabarCondi) / fCondi
+                rho
+                + mu_Y
+                + nu * (1 - beta)
+                - (sigma_Y ** 2 - sigma_Y * DeltabarCondi) / fCondi
         )
 
         mu_S_t = sigma_S * theta_t + r_t
         mu_S_st = (
-            mu_S_t + sigma_S * Delta_s_t
+                mu_S_t + sigma_S * Delta_s_t
         )  # expected stock return for agent born at t
         muhat_S_t = mu_S_t + sigma_S * np.sum(
             cohort_size * Delta_s_t
@@ -173,13 +194,217 @@ def simulate_cohorts(
         BIGPOPU[i] = popuCondi
 
         # Updating: (exogenous, for next period)
-        dDelta_s_t = (post_var(sigma_Y, Vhat, tau) / sigma_Y**2) * (
-            -Delta_s_t * dt + dZt[i]
+        dDelta_s_t = (post_var(sigma_Y, Vhat, tau) / sigma_Y ** 2) * (
+                -Delta_s_t * dt + dZt[i]
         )
         if i < Npre:
             DELbias = (np.sum(biasvec[i:]) + np.sum(dZt[:i])) / T_hat
         else:
-            DELbias = np.sum(dZt[i - Npre : i]) / T_hat
+            DELbias = np.sum(dZt[i - Npre: i]) / T_hat
+
+        Delta_s_t = Delta_s_t[1:] + dDelta_s_t[1:]
+        Delta_s_t = np.append(Delta_s_t, DELbias)
+        IntVec = reduction * Part[1:]
+        IntVec = np.append(IntVec, beta * (1 - reduction) * sumPart)
+
+    return (
+        Xt2,
+        part1,
+        mu_S,
+        mu_S_s,
+        mu_hat_S,
+        r,
+        theta,
+        BIGF,
+        BIGDELTA,
+        BIGMAX,
+        BIGPORT,
+        BIGPOPU,
+        BIGFCONDI,
+        BIGDELTABARCONDI,
+        dR,
+    )
+
+
+#####################################################################################
+
+def simulate_cohorts_complete_market(
+        biasvec: np.ndarray,
+        dZt: np.ndarray,
+        Nt: int,
+        Nc: int,
+        tau: np.ndarray,
+        IntVec: np.ndarray,
+        Delta_s_t: np.ndarray,
+        MaxThetaDelta_s_t: np.ndarray,
+        dt: float,
+        rho: float,
+        nu: float,
+        Vhat: float,
+        mu_Y: float,
+        sigma_Y: float,
+        sigma_S: float,
+        beta: float,
+        T_hat: float,
+        Npre: float,
+
+) -> Tuple[
+    np.ndarray,
+    np.ndarray,
+    np.ndarray,
+    np.ndarray,
+    np.ndarray,
+    np.ndarray,
+    np.ndarray,
+    np.ndarray,
+    np.ndarray,
+    np.ndarray,
+    np.ndarray,
+    np.ndarray,
+]:
+    """"" Simulate the economy forward
+
+    Args:
+        biasvec (np.ndarray): pre-trading period shocks to form beliefs of the initial cohort, shape(Npre,)
+        dZt (np.ndarray): random shocks, shape (Nt)
+        Nt (int): number of periods in the simulation
+        Nc (int): number of cohorts in the economy
+        tau (np.ndarray): t-s, shape(Nt)
+        IntVec (np.ndarray): ~similar to consumption share, shape(Nc)
+        Delta_s_t (np.ndarray): bias for each cohort, shape(Nc)
+        MaxThetaDelta_s_t (np.ndarray): max(Delta_), shape(Nc)
+        dt (float): per unit of time
+        rho (float): discount factor
+        nu (float): rate of birth and death
+        Vhat (float): initial variance
+        mu_Y (float): as in eq(1), drift of aggregate output growth
+        sigma_Y (float): as in eq(1), diffusion of aggregate output growth
+        sigma_S (float): as in eq(26), diffusion of stock price 
+        beta (float): as in eq(18), consumption share of the newborn cohort
+        T_hat (float): pre-trading years
+        Npre (float): pre-trading number of obs
+        mode (str): describes the mode of the func
+
+    Returns:
+        Xt2 (np.ndarray): xi_t * Yt, shape(Nt, )
+        part1 (np.ndarray): Consumption of each cohort, eq(16), where eta_s_t / eta_s_s follows eq(11), shape(Nc, )
+        mu_S (np.ndarray): expected return under true measure at t, shape(Nt, )
+        mu_S_s (np.ndarray): expected stock return each cohort, shape(Nt, Nc, )
+        mu_hat_S (np.ndarray): survey belief in the economy, shape(Nt, )
+        r (np.ndarray): interest rate, shape(Nt, )
+        theta (np.ndarray): market price of risk, shape(Nt, )
+        # muC_s_t (np.ndarray): drift of individual consumption, shape(Nc, )
+        # sigmaC_s_t (np.ndarray): diffusion of individual consumption, shape(Nc, )
+        BIGF (np.ndarray): consumption share over time, shape(Nt, Nc, )
+        BIGDELTA (np.ndarray): bias over time, shape(Nt, Nc, )
+        BIGMAX (np.ndarray): max(-theta, delta_s_t) over time, shape(Nt, Nc, )
+        BIGPORT (np.ndarray): portfolio choice over time, shape(Nt, Nc, )
+        BIGPOPU (np.ndarray): population that invest in stocks over time, shape(Nt, )
+        BIGFCONDI (np.ndarray): aggregate consumption share over time conditional on invest in stocks, shape(Nt, )
+        BIGDELTABARCONDI (np.ndarray): aggregate consumption weighted bias over time conditional on invest in stocks, shape(Nt, )
+        dR (np.ndarray): change of stock returns over time, shape(Nt, )
+
+    """ ""
+    # Initializing variables
+    Xt2 = np.ones(Nt)
+    # Deltabar2Conditional = np.ones(Nt)
+    # Et = np.ones(Nt)
+    # Vt = np.ones(Nt)
+
+    part1 = np.zeros(Nc)
+    dR = np.zeros(Nt)
+    reduction = np.exp(-nu * dt)
+    # BIGDELTA = np.zeros((Nt, Nc))
+    # BIGMAX = np.zeros((Nt, Nc))  # stores max(delta, -theta)
+    BIGF = np.zeros((Nt, Nc))
+    BIGPORT = np.zeros((Nt, Nc))
+    # BIGFCONDI = np.zeros((Nt))
+    # BIGDELTABARCONDI = np.zeros((Nt))
+    # BIGPOPU = np.zeros((Nt))
+
+    cohort = nu * np.exp(-nu * tau) * dt
+    population = np.sum(cohort)  # ~1
+    cohort_size = cohort / population
+
+    # Expected returns
+    mu_S = np.zeros(Nt)  # expected return under true measure
+    mu_S_s = np.zeros((Nt, Nc))  # expected return under agent-measure
+    mu_hat_S = np.zeros(Nt)  # average belief in the economy
+
+    # Consumption
+    # muC_s_t = np.zeros(Nt)  # drift log consumption
+    # sigmaC_s_t = np.zeros(Nt)  # diffusion log consumption
+
+    # Aggregate quantities
+    r = np.zeros(Nt)  # interest rate
+    theta = np.zeros(Nt)  # market price of risk
+
+    for i in tqdm(range(0, Nt)):
+
+        Part = IntVec * np.exp(
+            -(0.5 * MaxThetaDelta_s_t ** 2) * dt + MaxThetaDelta_s_t * dZt[i]
+        )
+
+        sumPart = np.sum(Part)
+        # Deltabar2Conditional[i] = np.sum(Part * MaxThetaDelta_s_t) / sumPart
+        f = Part / sumPart  # consumption share
+
+        # find theta
+        A = -np.max(Delta_s_t)
+        theta_t = bisection(
+            solve_theta, A, 10, f, Delta_s_t, sigma_Y
+        )
+        MaxThetaDelta_s_t = np.maximum(-theta_t, Delta_s_t)
+        invest = Delta_s_t >= -theta_t
+        invest_f = invest * f
+        popuCondi = np.sum(cohort_size * invest)
+        DeltabarCondi = np.sum(Delta_s_t * invest_f)
+        fCondi = np.sum(invest_f)
+
+        r_t = (
+                rho
+                + mu_Y
+                + nu * (1 - beta)
+                - (sigma_Y ** 2 - sigma_Y * DeltabarCondi) / fCondi
+        )
+
+        mu_S_t = sigma_S * theta_t + r_t
+        mu_S_st = (
+                mu_S_t + sigma_S * Delta_s_t
+        )  # expected stock return for agent born at t
+        muhat_S_t = mu_S_t + sigma_S * np.sum(
+            cohort_size * Delta_s_t
+        )  # survey average forecast
+
+        dR[i] = mu_S_t * dt + sigma_S * dZt[i]  # mu_t^Sdt + sigma_t^Sdz_t
+
+        # Et[i] = sum(f * (Vbar / (1 + (Vbar / sigma_Y ** 2) * dt * RevNt)) * (1 / sigma_Y))
+        # Vt[i] = (sum(f * Delta_s_t ** 2) - Deltabar2[i] ** 2) * sigma_Y
+
+        Port = np.maximum(Delta_s_t + theta_t, 0) / sigma_S
+
+        theta[i] = theta_t
+        r[i] = r_t
+        Xt2[i] = sumPart
+        mu_S[i] = mu_S_t
+        mu_S_s[i, :] = mu_S_st
+        mu_hat_S[i] = muhat_S_t
+        BIGF[i, :] = f
+        BIGDELTA[i, :] = Delta_s_t
+        BIGMAX[i, :] = MaxThetaDelta_s_t
+        BIGFCONDI[i] = fCondi
+        BIGDELTABARCONDI[i] = DeltabarCondi
+        BIGPORT[i] = Port
+        BIGPOPU[i] = popuCondi
+
+        # Updating: (exogenous, for next period)
+        dDelta_s_t = (post_var(sigma_Y, Vhat, tau) / sigma_Y ** 2) * (
+                -Delta_s_t * dt + dZt[i]
+        )
+        if i < Npre:
+            DELbias = (np.sum(biasvec[i:]) + np.sum(dZt[:i])) / T_hat
+        else:
+            DELbias = np.sum(dZt[i - Npre: i]) / T_hat
 
         Delta_s_t = Delta_s_t[1:] + dDelta_s_t[1:]
         Delta_s_t = np.append(Delta_s_t, DELbias)
