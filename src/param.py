c14accafd1506e0ff916488117320df9a65a5616
GoPenguinGo <75738992+GoPenguinGo@users.noreply.github.com>
2024-03-02 17:21:24 +0100

    try rho=0.1

--- src/param.py
+++ src/param.py
@@ -3,7 +3,7 @@ import numpy as np
 # Parameters
 rho = 0.001  # Time discount factor
 Ntype = 2
-rho_i = np.array([[0.001], [0.005]])  # baseline
+rho_i = np.array([[0.001], [0.1]])  # baseline
 # rho_i = np.array([[0.001], [0.05]])
 alpha_i = np.ones((Ntype, 1)) * 1 / Ntype
 nu = 0.02  # Death rate
