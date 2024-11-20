eb90e71c70174e67744564e96ef45f9dec085768
GoPenguinGo <75738992+GoPenguinGo@users.noreply.github.com>
2024-11-20 17:52:30 +0100

    reduce invest_mat

--- src/cohort_simulator.py
+++ src/cohort_simulator.py
@@ -143,7 +143,7 @@ def simulate_cohorts_SI(
 
     Phi_bar_parti = np.ones(Nt, dtype=np.float16)
     Phi_tilde_parti = np.ones(Nt, dtype=np.float16)
-    invest_mat = np.ones((Nt, Nc), dtype=np.int8)
+    invest_mat = np.ones((12, Nc), dtype=np.int8)
     parti_wealth_group = np.ones((Nt, 4), dtype=np.float16)
     parti_age_group = np.ones((Nt, 4), dtype=np.float16)
 
@@ -348,7 +348,6 @@ def simulate_cohorts_SI(
             Phi_bar_parti[i] = fc_parti_t
             Phi_tilde_parti[i] = fw_parti_t
             parti[i] = popu_parti_t
-            invest_mat[i] = invest_tracker[0]
             for j in range(4):
                 parti_age_group[i, j] = np.ma.average(invest_tracker[:, cutoffs_age[j + 1]:cutoffs_age[j]],
                                                       weights=cohort_type_size[:, cutoffs_age[j + 1]:cutoffs_age[j]])
@@ -357,10 +356,11 @@ def simulate_cohorts_SI(
                                                          weights=cohort_type_size[within_group])
 
         entry_i = np.copy(invest_tracker[0])
-        entry_i[:-12] = invest_tracker[0, :-12] > invest_mat[i - 12, 12:]  # entry including the newborns who are in
-        exit_i = invest_tracker[0, :-12] < invest_mat[i - 12, 12:]
+        entry_i[:-12] = invest_tracker[0, :-12] > invest_mat[0, 12:]  # entry including the newborns who are in
+        exit_i = invest_tracker[0, :-12] < invest_mat[0, 12:]
         entry_mat[i] = np.average(entry_i, weights=np.sum(cohort_type_size, axis=0))
         exit_mat[i] = np.average(exit_i, weights=np.sum(cohort_type_size[:, :-12], axis=0))
+        invest_mat = np.append(invest_mat[1:], np.reshape(invest_tracker[0], (1, -1)), axis=0)
 
     return (
         r,
@@ -993,7 +993,7 @@ def simulate_cohorts_mix_type(
         pi = np.zeros((Nt, Nconstraint, Nc), dtype=np.float16)  # portfolio choices
     else:
         pi = 0
-    invest_mat = np.zeros((Nt, Nconstraint, Nc), dtype=np.int8)
+    invest_mat = np.zeros((12, Nconstraint, Nc), dtype=np.int8)
 
     for i in tqdm(range(Nt)):
         dZ_t = dZ[i]
@@ -1133,13 +1133,12 @@ def simulate_cohorts_mix_type(
         Phi_can_short[i] = Phi_can_short_t
         parti[i] = popu_parti_t
 
-        invest_mat[i] = invest_tracker[0]
-        # turnover = invest_tracker[0, :, 12:] - invest_mat[i - 12, :, :-12]
         entry_i = np.copy(invest_tracker[0])
-        entry_i[:, :-12] = invest_tracker[0, :, :-12] > invest_mat[i - 12, :, 12:]  # entry including the newborns who are in
-        exit_i = invest_tracker[0, :, :-12] < invest_mat[i - 12, :, 12:]  # exit excluding the newborns
+        entry_i[:, :-12] = invest_tracker[0, :, :-12] > invest_mat[0, :, 12:]  # entry including the newborns who are in
+        exit_i = invest_tracker[0, :, :-12] < invest_mat[0, :, 12:]  # exit excluding the newborns
         entry_mat[i] = np.average(entry_i, weights=np.sum(cohort_type_size_mix, axis=0))
         exit_mat[i] = np.average(exit_i, weights=np.sum(cohort_type_size_mix[:, :, :-12], axis=0))
+        invest_mat = np.copy(np.append(invest_mat[1:], np.reshape(invest_tracker[0], (1, 4,-1)), axis=0))
 
         for j in range(4):
             parti_age_group[i, j] = np.ma.average(invest_tracker[:, :, cutoffs_age[j + 1]:cutoffs_age[j]],
@@ -1159,6 +1158,279 @@ def simulate_cohorts_mix_type(
         Phi_tilde_parti,
         Delta_bar_parti,
         Delta_tilde_parti,
+        dR,
+        mu_S,
+        sigma_S,
+        beta,
+        invest_mat,
+        parti_age_group,
+        parti_wealth_group,
+        entry_mat,
+        exit_mat
+    )
+
+
+def simulate_cohorts_mix_impulse(
+        biasvec: np.ndarray,
+        dZ: np.ndarray,
+        dZ_SI: np.ndarray,
+        Nt: int,
+        Nc: int,
+        Nr: int,
+        dt: float,
+        Ntype: int,
+        Nconstraint: int,
+        rho_i: np.ndarray,
+        alpha_i: np.ndarray,
+        beta_i: np.ndarray,
+        rho_cohort_type: np.ndarray,
+        beta0: float,
+        nu: float,
+        Vhat: float,
+        mu_Y: float,
+        sigma_Y: float,
+        tax: float,
+        phi: float,
+        T_hat: float,
+        Npre: float,
+        cohort_type_size: np.ndarray,
+        cutoffs_age: np.ndarray,
+        Delta_s_t: np.ndarray,
+        eta_st_eta_ss: np.ndarray,
+        X: np.ndarray,
+        d_eta_st: np.ndarray,
+        invest_tracker: np.ndarray,
+        can_short_tracker: np.ndarray,
+        tau_info: np.ndarray,
+        Vhat_vector: np.ndarray,
+) -> Tuple[
+    np.ndarray,
+    np.ndarray,
+    np.ndarray,
+    np.ndarray,
+    np.ndarray,
+    np.ndarray,
+    np.ndarray,
+    np.ndarray,
+    np.ndarray,
+    np.ndarray,
+    np.ndarray,
+    np.ndarray,
+]:
+    """ Simulate the economy forward
+        a mixture of 4 different types of agents in each cohort:
+        unconstrained (designated participants);
+        excluded (designated nonparticipants);
+        disappointment (leave the stock market for good upon binding constraint);
+        & reentry.
+
+    Args:
+        biasvec (np.ndarray): shocks to the output in the build stage for the cohorts born between s=[0, Npre]
+        dZ (np.ndarray): shocks to the output for each period, shape (Nt, )
+        dZ_SI(np.ndarray): shocks to the signal for each period, shape (Nt, )
+        Nt (int): number of periods in the simulation
+        Nc (int): number of cohorts in the economy
+        tau (np.ndarray): t-s, shape(Nt)
+        dt (float): per unit of time
+        Ntype (int): number of types for time preference
+        rho_i (np.ndarray): time preference of each type
+        alpha_i (np.ndarray): density of each type
+        beta_i (np.ndarray): consumption wealth ratio of each type
+        rho_cohort_type (np.ndarray): alpha_i * exp(-(rho_i + nu)*(t-s))
+        beta0 (float): average consumption wealth ratio of a new born cohort
+        nu (float): rate of birth and death
+        Vhat (float): initial variance
+        mu_Y (float): as in eq(1), drift of aggregate output growth
+        sigma_Y (float): as in eq(1), diffusion of aggregate output growth
+        tax (float): marginal rate of wealth tax
+        phi (float): correlation between the signal and the output growth rate
+        T_hat (float): pre-trading years
+        Npre (float): pre-trading number of observations
+        cohort_type_size (np.ndarray): cohort_size * alpha_i
+        cutoffs_age (np.ndarray): cutoff ages for the age quartiles
+        -*-*-*- from the cohort_builder function -*-*-*-
+        Delta_s_t (np.ndarray): estimation bias, shape(Ntype, Nc, )
+        eta_st_eta_ss(np.ndarray): shape(Ntype, Nc, )
+        X(np.ndarray):W_s * Xi_s, shape(Ntype, Nc, )
+        d_eta_st (np.ndarray): max(delta_st, -theta_t), shape(Ntype, Nc, )
+        invest_tracker (np.ndarray): track if a cohort is still in the stock market, shape(Ntype, Nc, )
+        tau_info (np.ndarray): t-t', time since the last time a cohort switch, shape(Ntype, Nc, )
+        Vhat_vector (np.ndarray): variance at t', shape(Ntype, Nc, )
+        -*-*-*- avoid saving results unnecessary for particular analysis -*-*-*-
+        need_f (str): {'True', 'False'}
+        need_Delta (str): {'True', 'False'}
+        need_pi (str): {'True', 'False'}
+
+    Returns:
+        r (np.ndarray): interest rate, shape(Nt, )
+        theta (np.ndarray): market price of risk, shape(Nt, )
+        f_c (np.ndarray): consumption share, shape(Nt, Ntype, Nc)
+        Delta (np.ndarray): standardized estimation error, shape(Nt, Ntype, Nc)
+        pi (np.ndarray): portfolio, shape(Nt, Ntype, Nc)
+        parti (np.ndarray): participation rate, shape(Nt)
+        Phi_bar_parti (np.ndarray): consumption share of participants, shape(Nt)
+        Phi_tilde_parti (np.ndarray): wealth share of participants, shape(Nt)
+        Delta_bar_parti (np.ndarray): consumption weighted average estimation error of participants, shape(Nt)
+        Delta_tilde_parti (np.ndarray): wealth weighted average estimation error of participants, shape(Nt)
+        dR (np.ndarray): realized stock returns, shape(Nt)
+        mu_S (np.ndarray): expected equity risk premia, shape(Nt)
+        sigma_S (np.ndarray): conditional volatility of stock returns, shape(Nt)
+        beta (np.ndarray): aggregate consumption wealth ratio, shape(Nt)
+        invest_mat (np.ndarray): whether a cohort is in the stock market, shape(Nt, Nc)
+        parti_age_group: participation rate in age groups, shape(Nt, 4)
+        parti_wealth_group: participation rate in wealth groups, shape(Nt, 4)
+
+    """
+    a_phi = (1 - phi ** 2)
+    phi_sqr_a_phi = phi / np.sqrt(a_phi)
+    a_phi_1 = 1 / a_phi
+    sigma_Y_sq = sigma_Y ** 2
+    wealth_cutoffs = np.array([0, 1, 10, 100, 100000])
+
+    # Initializing variables
+    invest_newborn = np.array([[[1], [0], [1], [1]]]) * np.ones((Ntype, Nconstraint, 1), dtype=np.int8)
+    can_short_newborn = np.array([[[1], [0], [0], [0]]]) * np.ones((Ntype, Nconstraint, 1), dtype=np.int8)
+    append_init = np.ones((Ntype, Nconstraint, 1), dtype=np.int8)
+    invest_mat = np.zeros((12, Nconstraint, Nc), dtype=np.int8)
+    # equilibrium terms:
+    dR = np.zeros((3, Nr))  # stores stock returns
+    r = np.zeros((3, Nr))  # interest rate
+    theta = np.zeros((3, Nr))  # market price of risk
+    mu_S = np.zeros((3, Nr))
+    sigma_S = np.zeros((3, Nr))
+    beta = np.zeros((3, Nr))
+    parti = np.ones((3, Nr), dtype=np.float16)  # participation rate
+    entry_mat = np.ones((3, Nr), dtype=np.float16)
+    exit_mat = np.ones((3, Nr), dtype=np.float16)
+    parti_wealth_group = np.zeros((3, Nr, 4), dtype=np.float16)
+    parti_age_group = np.zeros((3, Nr, 4), dtype=np.float16)
+
+    dR_t = 0
+
+    for i in tqdm(range(Nt)):
+        dZ_t = dZ[i]
+        dZ_SI_t = dZ_SI[i]
+
+        # new cohort born (age 0), get wealth transfer, observe, invest
+        eta_st_eta_ss = eta_st_eta_ss * np.exp(
+            (-0.5 * d_eta_st ** 2) * dt
+            + d_eta_st * dZ_t
+        )  # equation (15)
+
+        X_parts = tax * X * eta_st_eta_ss * rho_cohort_type * dt
+        X_t = np.sum(X_parts) / (1 - tax * beta0 * dt)
+
+        eta_st_eta_ss = np.append(eta_st_eta_ss[:, :, 1:], append_init, axis=2)
+        X = np.append(X[:, :, 1:], X_t * np.ones((1, 1, 1)), axis=2)
+        X = X / X_t  # rescale, does not change the relative magnitude of each cohort
+
+        f_c_ist = X_parts / X_t / dt
+        f_c_ist = np.append(f_c_ist[:, :, 1:], tax * alpha_i * beta_i, axis=2)
+
+        beta_t = 1 / np.sum(f_c_ist / beta_i * dt)
+        f_w_ist = f_c_ist / beta_i * beta_t
+
+        w_indiv_ist = f_w_ist / cohort_type_size * dt
+        if i > 0:
+            dR_t = mu_S_t * dt + sigma_S_t * dZ_t
+
+        # update beliefs
+        V_st_N = post_var(sigma_Y_sq, Vhat_vector, tau_info, a_phi, 'N')  # from eq(6)
+        dDelta_s_t_N = dDelta_st_calculator(sigma_Y_sq, a_phi_1, phi_sqr_a_phi, dt, V_st_N, Delta_s_t, dZ_t, dZ_SI_t,
+                                            'N')  # from eq(9)
+        V_st_P = post_var(sigma_Y_sq, Vhat_vector, tau_info, a_phi, 'P')
+        dDelta_s_t_P = dDelta_st_calculator(sigma_Y_sq, a_phi_1, phi_sqr_a_phi, dt, V_st_P, Delta_s_t, dZ_t, dZ_SI_t,
+                                            'P')
+        dDelta_s_t = invest_tracker * dDelta_s_t_P + (
+                1 - invest_tracker) * dDelta_s_t_N  # the participation decision of last time affects the updating pattern
+        tau_info = np.append(tau_info[:, :, 1:], 0 * append_init, axis=2) + dt
+        Vhat_vector = np.append(Vhat_vector[:, :, 1:], Vhat * append_init, axis=2)
+
+        if i < Npre - 1:
+            init_bias = (np.sum(biasvec[i + 1:]) + np.sum(dZ[:i + 1])) / T_hat
+        else:
+            init_bias = np.sum(dZ[i + 1 - Npre:i + 1]) / T_hat
+
+        Delta_s_t = Delta_s_t[:, :, 1:] + dDelta_s_t[:, :, 1:]
+        Delta_s_t = np.append(Delta_s_t, init_bias * append_init, axis=2)
+
+        # find the market clearing theta, given beliefs and consumption shares of cohorts in the economy
+        invest_tracker = np.append(invest_tracker[:, :, 1:], invest_newborn,
+                                   axis=2)  # all cohorts that are still in the market, 1 by default
+        can_short_tracker = np.append(can_short_tracker[:, :, 1:], can_short_newborn, axis=2)
+
+        possible_cons_share = f_c_ist * dt * invest_tracker
+        possible_delta_st = Delta_s_t * invest_tracker
+
+        lowest_bound = -np.max(possible_delta_st[np.nonzero(possible_delta_st)])  # absolute lower bound
+        theta_t = bisection_partial_constraint(
+            solve_theta_partial_constraint, lowest_bound, 50, can_short_tracker, possible_delta_st, possible_cons_share,
+            sigma_Y
+        )
+        a = Delta_s_t + theta_t
+        invest = 1 - (a < 0) * (can_short_tracker < 1)  # not invest if a<0 and can not short
+        invest[:, 1] = 0  # exclusion type
+        switch_P_to_N = invest_tracker * (1 - invest) * (
+                can_short_tracker < 1)  # switch to nonparti if type R&E & not investing this period
+        switch_N_to_P = np.maximum(invest - invest_tracker,
+                                   0)  # switch to parti if not investing before & investing this period
+        switch_N_to_P[:, :3] = 0  # only applicable to the E type
+        switch = switch_N_to_P + switch_P_to_N
+        invest_tracker = invest_tracker + switch_N_to_P - switch_P_to_N
+        d_eta_st = a * invest_tracker - theta_t
+
+        # tau_info and V_hat has to change for the agents who switch (either P to N or vice versa)
+        Vhat_vector = np.append(V_st_P[:, :, 1:], Vhat * append_init, axis=2) * switch_P_to_N + \
+                      np.append(V_st_N[:, :, 1:], Vhat * append_init, axis=2) * switch_N_to_P + \
+                      Vhat_vector * (1 - switch)  # reset V'
+        tau_info = dt * switch + tau_info * (1 - switch)  # reset t'
+
+        invest_fw_st = invest_tracker * f_w_ist * dt
+        popu_parti_t = np.sum(cohort_type_size * invest_tracker)
+        fw_parti_t = np.sum(invest_fw_st)
+        Delta_tilde_parti_t = np.sum(Delta_s_t * invest_fw_st) / fw_parti_t
+        sigma_S_t = fw_parti_t * (theta_t + Delta_tilde_parti_t)
+        rho_bar_t = np.sum(rho_i * f_c_ist) / np.sum(f_c_ist)
+
+        r_t = (
+                nu - tax * beta0
+                + rho_bar_t
+                + mu_Y
+                - sigma_Y * theta_t
+        )
+
+        mu_S_t = sigma_S_t * theta_t + r_t
+
+    for j in range(Nr):
+
+        # store the results
+        dR[i] = dR_t  # realized return from t-1 to t
+        theta[i] = theta_t
+        r[i] = r_t
+        mu_S[i] = mu_S_t
+        sigma_S[i] = np.abs(sigma_S_t)  # stock vola = absolute value of sigma
+        beta[i] = beta_t
+        parti[i] = popu_parti_t
+
+        # turnover = invest_tracker[0, :, 12:] - invest_mat[i - 12, :, :-12]
+        entry_i = np.copy(invest_tracker[0])
+        entry_i[:, :-12] = invest_tracker[0, :, :-12] > invest_mat[0, :, 12:]  # entry including the newborns who are in
+        exit_i = invest_tracker[0, :, :-12] < invest_mat[0, :, 12:]  # exit excluding the newborns
+        entry_mat[i] = np.average(entry_i, weights=np.sum(cohort_type_size_mix, axis=0))
+        exit_mat[i] = np.average(exit_i, weights=np.sum(cohort_type_size_mix[:, :, :-12], axis=0))
+        invest_mat = np.copy(np.append(invest_mat[1:], np.reshape(invest_tracker[0], (1, 4, -1)), axis=0))
+
+        for j in range(4):
+            parti_age_group[i, j] = np.ma.average(invest_tracker[:, :, cutoffs_age[j + 1]:cutoffs_age[j]],
+                                                  weights=cohort_type_size[:, :, cutoffs_age[j + 1]:cutoffs_age[j]])
+            within_group = np.where((w_indiv_ist >= wealth_cutoffs[j]) * (w_indiv_ist < wealth_cutoffs[j + 1]))
+            parti_wealth_group[i, j] = np.ma.average(invest_tracker[within_group],
+                                                     weights=cohort_type_size[within_group])
+
+    return (
+        r,
+        theta,
+        parti,
         dR,
         mu_S,
         sigma_S,
