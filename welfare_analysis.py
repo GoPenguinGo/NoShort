20183bcee42ce2d1f886f1b7b431ac3684ea3d17
GoPenguinGo <75738992+GoPenguinGo@users.noreply.github.com>
2024-12-04 18:04:32 +0100

    change rho

--- welfare_analysis.py
+++ welfare_analysis.py
@@ -17,17 +17,20 @@ c_rho_bar = alpha_i[0, 0] * tax * beta_i[0, 0] * (rho_i[0, 0] - rho_i[1, 0]) + r
 rho_bar = (-b_rho_bar - np.sqrt(b_rho_bar ** 2 - 4 * a_rho_bar * c_rho_bar)) / (2 * a_rho_bar)
 T = 500
 N_T = int(T / dt)
-# mode_trade = "w_constraint"
-mode_trade = "complete"
+mode_trade = "w_constraint"
+# mode_trade = "complete"
 mode_learn = 'reentry'
-Mpath = 1000
+Mpath = 2000
 Nt_long = 8400
-T_hat_vec = np.linspace(2, 20, 10)
+T_hat_vec = np.append(
+    np.arange(1, 4, 1),
+    np.arange(4, 30, 3),
+)
 t_s_mat = np.tile(np.reshape(np.cumsum(np.ones(N_T) * dt) - dt, (-1, 1)), (1, 2))
 rho_i_mat = np.reshape(rho_i, (1, -1))
 discount_rate_mat = np.exp(-(nu + rho_i_mat) * t_s_mat)
 folder_address = r'E:\Users\A2010290\Documents\GitHub\NoShort/welfare/'
-
+plt.rcParams["font.family"] = 'serif'
 
 def utility_mu(mu_Y_use):
     t_s = np.cumsum(np.ones(N_T) * dt) - dt
@@ -124,7 +127,7 @@ def simulate_path(
 
 
 def main():
-    for T_hat_try in T_hat_vec:
+    for T_hat_try in T_hat_vec[1:3]:
         with ProcessPoolExecutor(max_workers=25) as executor:  # Adjust the number of workers as needed
             results = [executor.submit(simulate_path, i, int(T_hat_try)) for i in range(Mpath)]
             # Initialize a list to store the results
@@ -145,43 +148,43 @@ def main():
         # Create a DataFrame from the list of dictionaries
         results_df = pd.DataFrame(results_list)
         results_dict = results_df.to_dict(orient='list')
-        np.savez(folder_address + str(int(T_hat_try)) + "complete.npz", **results_dict)
+        np.savez(folder_address + str(int(T_hat_try)) + ".npz", **results_dict)
 
 
 if __name__ == '__main__':
     main()
 
-    N_points = 200
-    mu_vec = np.linspace(0, mu_Y, N_points)
-    E_util_mu = np.zeros((N_points, 2))
-    for i, mu_try in enumerate(mu_vec):
-        E_util_mu[i] = utility_mu(mu_try)
-
-    t_s_mat = np.tile(np.reshape(np.cumsum(np.ones(N_T) * dt) - dt, (-1, 1)), (1, 2))
-    rho_i_mat = np.reshape(rho_i, (1, -1))
-    discount_rate_mat = np.exp(-(nu + rho_i_mat) * t_s_mat)
-
-    equiv_mu = np.zeros((len(T_hat_vec), 2))
-
-    for i, T_hat_try in enumerate(T_hat_vec):
-        E_util_learn_path = np.load(folder_address + str(int(T_hat_try)) + "all_runs.npz")['E_util']
-        E_util_learn = np.average(np.ma.masked_invalid(E_util_learn_path), axis=0)
-        for j in range(2):
-            equiv_mu[i, j] = mu_vec[np.searchsorted(E_util_mu[:, j], E_util_learn[j])]
-
-    fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(7, 4))
-    ax.set_xlabel('Pre-entry learning window')
-    ax.set_ylabel(r'Equivalent $\mu^Y$')
-    ax.set_ylim(0, 0.025)
-    ax.plot(T_hat_vec, equiv_mu[:, 0], color='navy', linewidth=1.5, label=r'type 1, $\rho=0.001$')
-    ax.plot(T_hat_vec, equiv_mu[:, 1], color='red', linewidth=1.5, label=r'type 2, $\rho=0.005$')
-    plt.axhline(y=0.02, color='gray', linestyle='dashed', label=r'Actual $\mu^Y$')
-    plt.legend()
-    ax.tick_params(axis='y', labelcolor='black')
-    fig.tight_layout()  # otherwise the right y-label is slightly clipped
-    plt.savefig('Welfare.png', dpi=100)
-    plt.show()
-    plt.close()
+    # N_points = 200
+    # mu_vec = np.linspace(0, mu_Y, N_points)
+    # E_util_mu = np.zeros((N_points, 2))
+    # for i, mu_try in enumerate(mu_vec):
+    #     E_util_mu[i] = utility_mu(mu_try)
+    #
+    # t_s_mat = np.tile(np.reshape(np.cumsum(np.ones(N_T) * dt) - dt, (-1, 1)), (1, 2))
+    # rho_i_mat = np.reshape(rho_i, (1, -1))
+    # discount_rate_mat = np.exp(-(nu + rho_i_mat) * t_s_mat)
+    #
+    # equiv_mu = np.zeros((len(T_hat_vec), 2))
+    #
+    # for i, T_hat_try in enumerate(T_hat_vec):
+    #     E_util_learn_path = np.load(folder_address + str(int(T_hat_try)) + ".npz")['E_util']
+    #     E_util_learn = np.average(np.ma.masked_invalid(E_util_learn_path), axis=0)
+    #     for j in range(2):
+    #         equiv_mu[i, j] = mu_vec[np.searchsorted(E_util_mu[:, j], E_util_learn[j])]
+    #
+    # fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(7, 4))
+    # ax.set_xlabel('Pre-entry learning window')
+    # ax.set_ylabel(r'Equivalent $\mu^Y$')
+    # ax.set_ylim(0, 0.025)
+    # ax.plot(T_hat_vec, equiv_mu[:, 0], color='navy', linewidth=1.5, label=r'type 1, $\rho=0.001$')
+    # ax.plot(T_hat_vec, equiv_mu[:, 1], color='red', linewidth=1.5, label=r'type 2, $\rho=-0.001$')
+    # plt.axhline(y=0.02, color='gray', linestyle='dashed', label=r'Actual $\mu^Y$')
+    # plt.legend()
+    # ax.tick_params(axis='y', labelcolor='black')
+    # fig.tight_layout()  # otherwise the right y-label is slightly clipped
+    # plt.savefig('Welfare.png', dpi=100)
+    # plt.show()
+    # plt.close()
 
 
 
