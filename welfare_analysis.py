e275da2145c7ea06c84cc2d4017f0f8e24918eaa
GoPenguinGo <75738992+GoPenguinGo@users.noreply.github.com>
2026-05-13 19:38:16 +0200

    Update welfare_analysis.py

--- welfare_analysis.py
+++ welfare_analysis.py
@@ -1,29 +1,25 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from src.simulation import simulate_SI, simulate_mix_types
+from src.simulation import simulate_mix_types
 from src.param import mu_Y, sigma_Y, \
     dt, Ninit, Nc, Nt, tau, \
     cutoffs_age, Ntype, alpha_i, \
     dZ_build_matrix, dZ_SI_build_matrix, dZ_SI_matrix, dZ_matrix, \
     cohort_size, rho_i, tax, beta0, beta_i, nu, Vhat, phi, Npre, \
-    T_hat, rho_cohort_type, cohort_type_size, entry_bound, exit_bound
+    T_hat, rho_cohort_type, cohort_type_size, entry_bound, exit_bound, phi_vector
 from concurrent.futures import ProcessPoolExecutor
-from src.param_mix import Nconstraint, rho_i_mix
+from src.param_mix import Nconstraint, rho_i_mix, density
 from scipy.interpolate import make_interp_spline
 
-density = (0.3, 0.5, 0.2) #norway
 
 a_rho_bar = 1
 b_rho_bar = -(tax * beta0 + rho_i[0, 0] + rho_i[1, 0])
 c_rho_bar = alpha_i[0, 0] * tax * beta_i[0, 0] * (rho_i[0, 0] - rho_i[1, 0]) + rho_i[1, 0] * rho_i[0, 0] + tax * beta0 * \
             rho_i[1, 0]
 rho_bar = (-b_rho_bar - np.sqrt(b_rho_bar ** 2 - 4 * a_rho_bar * c_rho_bar)) / (2 * a_rho_bar)
-T = 500
+T = 400
 N_T = int(T / dt)
-mode_trade = "w_constraint"
-# mode_trade = "complete"
-mode_learn = 'reentry'
 Mpath = 2000
 # Nt_long = 8400
 Nt_long = 6000
@@ -32,7 +28,8 @@ c_sample = np.arange(-1, -int(200/dt), -24)
 fc_init = tax / (1 + tax) * (1 + rho_i / nu)
 growth_c_i = nu - tax * beta0 + rho_bar - rho_i
 f_c_benchmark = (fc_init * np.exp(growth_c_i * tau))[:, c_sample]
-
+keep = int(200 / dt)
+sample = np.arange(0, N_T, 1)
 # T_hat_vec = np.append(
 #     np.arange(1, 5, 2),
 #     np.arange(5, 30, 5),
@@ -40,18 +37,10 @@ f_c_benchmark = (fc_init * np.exp(growth_c_i * tau))[:, c_sample]
 T_hat_vec = [2, 5, 10, 20]
 t_s_mat = np.tile(np.reshape(np.cumsum(np.ones(N_T) * dt) - dt, (-1, 1)), (1, 2))
 rho_i_mat = np.reshape(rho_i, (1, -1))
-discount_rate_mat = np.exp(-(nu + rho_i_mat) * t_s_mat)
+discount_rate_mat = np.exp(-(nu + rho_i_mat) * t_s_mat)[:N_T]
 folder_address = r'E:\Users\A2010290\Documents\GitHub\NoShort/welfare/'
 plt.rcParams["font.family"] = 'serif'
 
-age = T - np.cumsum(np.ones(N_T) * dt)
-xi_C = -rho_i + nu - tax * beta0 + rho_bar
-flow = np.sum(
-    cohort_size * (
-            np.sum(np.log(beta_i) * alpha_i)
-            + alpha_i * xi_C * age
-    )
-)
 
 
 def utility_mu(mu_Y_use, sigma_Y_use):
@@ -155,18 +144,19 @@ def utility_mu(mu_Y_use, sigma_Y_use):
 def simulate_path(
         i: int,
         T_hat_use,
+        phi_use,
         density,
 ):
     print(i)
     # shocks
     dZ_build = dZ_build_matrix[i]
     dZ = np.random.randn(Nt_long) * np.sqrt(dt)
-    dZ_SI_build = dZ_SI_build_matrix[i]
-    dZ_SI = np.random.randn(Nt_long) * np.sqrt(dt)
-    keep = int(200/dt)
 
     Npre_use = int(T_hat_use / dt)
     Vhat_use = (sigma_Y ** 2) / T_hat_use  # prior variance
+
+    log_C_mat = np.zeros((10, int(T / dt), Ntype, Nconstraint), dtype=np.float32)
+    cohort_size = cohort_type_size_mix[0, 0]
 
     alpha_constraint = np.ones(
         (1, Nconstraint)) * density
@@ -175,9 +165,6 @@ def simulate_path(
     beta_i_mix = (nu + rho_i_mix) / (1 + tax)  # consumption wealth ratio
     rho_cohort_type_mix = alpha_i_mix * beta_i_mix * np.exp(
         -(rho_i_mix + nu) * tau)  # shape(2, 6000)
-
-    log_C_mat = np.zeros((10, int(T / dt), 2), dtype=np.float32)
-    sample = np.arange(0, N_T, 1)
 
     (
         r,
@@ -194,22 +181,21 @@ def simulate_path(
         mu_S,
         sigma_S,
         beta,
-        invest_tracker,
         parti_age_group,
-        parti_wealth_group,
+        # Delta_popu,
+        # parti_wealth_group,
         entry_mat,
         exit_mat
     ) = simulate_mix_types(Nc, Nt, dt, nu,
                            Vhat_use,
-                           mu_Y, sigma_Y, tax,
-                           beta0,
-                           phi,
+                           mu_Y, sigma_Y, tax, beta0,
+                           phi_use,
                            Npre_use,
                            Ninit,
                            T_hat_use,
                            entry_bound,
                            exit_bound,
-                           dZ_build, dZ, dZ_SI_build, dZ_SI,
+                           dZ_build, dZ,
                            cutoffs_age, Ntype,
                            Nconstraint, rho_i_mix, alpha_i_mix, beta_i_mix,
                            rho_cohort_type_mix,
@@ -217,40 +203,34 @@ def simulate_path(
                            need_f='True',
                            need_Delta='True',
                            need_pi='True',
+                           mode_learn='invest',
                            )
 
     if density[-1] == 0:
         C_share = f_c[:, :, 0] / cohort_type_size_mix[:, 0] * dt
-        flow = np.sum(
-            np.sum(np.log(C_share) * cohort_type_size_mix[:, 0], axis=2), axis=1
-        )
+        flow = np.average(np.average(np.log(C_share), weights=cohort_size, axis=2), weights=alpha_i[:, 0], axis=1)
 
     else:
-        C_share = np.maximum(f_c / cohort_type_size_mix * dt, 1e-5)
-        flow = np.sum(
-            np.sum(
-                np.sum(np.log(C_share) * cohort_type_size_mix, axis=3), axis=2), axis=1
-        )
+        C_share = np.maximum(f_c / cohort_type_size_mix * dt, 1e-5)  # per unit of agents
+        flow = np.average(np.average(np.log(C_share), weights=cohort_size, axis=3), weights=alpha_i[:, 0], axis=1)
 
         C_mat = np.exp((mu_Y - 1 / 2 * sigma_Y ** 2) * np.arange(0, Nt_long) * dt + sigma_Y * np.cumsum(dZ))
 
         for j in range(10):
-            t_start = int(100 / dt) + int(10 / dt) * j
+            t_start = int(10 / dt) * j
             log_C_mat[j] = np.log(
-                C_share[t_start + sample, :, Nt - 1 - sample] / C_share[t_start, :, -1]
-            ) * np.log(
+                C_share[t_start + sample, :, :, Nt - 1 - sample] / C_share[t_start, :, :, -1]
+            ) * np.reshape(np.log(
                 C_mat[t_start + sample] / C_mat[t_start]
-            )
+            ), (-1, 1, 1))
 
         # np.save(folder_address + str(i) + ".npy", np.average(log_C_mat, axis=0))
-        E_util_path_t = np.average(log_C_mat, axis=0) * dt * discount_rate_mat
-        E_util_path = np.sum(E_util_path_t, axis=0)
-
-        # todo: do we care about the distribution between types?
+        E_util_path_t = np.average(log_C_mat, axis=0) * np.reshape(discount_rate_mat, (-1, Ntype, 1))
+        E_util_path = np.sum(E_util_path_t * dt, axis=0)
 
     return (
         i,
-        np.average(flow[-keep:]),
+        flow[-keep:],
         E_util_path,
     )
 
@@ -258,63 +238,65 @@ def simulate_path(
 
 def main():
     for T_hat_try in T_hat_vec:
-        with ProcessPoolExecutor(max_workers=20) as executor:  # Adjust the number of workers as needed
-            results = [executor.submit(simulate_path, i, int(T_hat_try), density) for i in range(Mpath)]
-        results_list = []
+        for phi in phi_vector:
+            with ProcessPoolExecutor(max_workers=20) as executor:  # Adjust the number of workers as needed
+                results = [executor.submit(simulate_path, i, int(T_hat_try), density, phi) for i in range(Mpath)]
+            results_list = []
 
-        for result in results:
-            i, \
-                flow_welfare, = result.result()
+            for result in results:
+                i, \
+                    flow_welfare, \
+                    E_util = result.result()
 
-            data = {
-                'i': i,
-                'flow_welfare': flow_welfare,
-            }
-            results_list.append(data)
+                data = {
+                    'i': i,
+                    'flow_welfare': flow_welfare,
+                    'E_util': E_util,
+                }
+                results_list.append(data)
 
-        results_df = pd.DataFrame(results_list)
-        results_dict = results_df.to_dict(orient='list')
-        np.savez(folder_address + str(int(T_hat_try)) + "_wel_flow.npz", **results_dict)
+            results_df = pd.DataFrame(results_list)
+            results_dict = results_df.to_dict(orient='list')
+            np.savez(folder_address + f"{int(T_hat_try)}_{phi}_wel_flow.npz", **results_dict)
 
+            # with ProcessPoolExecutor(max_workers=20) as executor:  # Adjust the number of workers as needed
+            #     results = [executor.submit(simulate_path, i, int(T_hat_try)) for i in range(Mpath)]
+            # results_list = []
+            #
+            # for result in results:
+            #     i, \
+            #     f_c_result,  = result.result()
+            #
+            #     data = {
+            #         'i': i,
+            #         'f_c_indi': f_c_result,
+            #     }
+            #     results_list.append(data)
+            #
+            # results_df = pd.DataFrame(results_list)
+            # results_dict = results_df.to_dict(orient='list')
+            # np.savez(folder_address + str(int(T_hat_try)) + "_indi.npz", **results_dict)
 
-        ### the complete one
-        with ProcessPoolExecutor(max_workers=20) as executor:  # Adjust the number of workers as needed
-            results = [executor.submit(simulate_path, i, 10, (1, 0, 0)) for i in range(Mpath)]
-        results_list = []
+    ### the complete one
+    with ProcessPoolExecutor(max_workers=20) as executor:  # Adjust the number of workers as needed
+        results = [executor.submit(simulate_path, i, 10, (1, 0, 0)) for i in range(Mpath)]
+    results_list = []
 
-        for result in results:
-            i, \
-                flow_welfare, = result.result()
+    for result in results:
+        i, \
+            flow_welfare, \
+            E_util = result.result()
 
-            data = {
-                'i': i,
-                'flow_welfare': flow_welfare,
-            }
-            results_list.append(data)
+        data = {
+            'i': i,
+            'flow_welfare': flow_welfare,
 
-        results_df = pd.DataFrame(results_list)
-        results_dict = results_df.to_dict(orient='list')
-        np.savez(folder_address + "benchmark_wel_flow.npz", **results_dict)
+        }
+        results_list.append(data)
 
-
-
-        # with ProcessPoolExecutor(max_workers=20) as executor:  # Adjust the number of workers as needed
-        #     results = [executor.submit(simulate_path, i, int(T_hat_try)) for i in range(Mpath)]
-        # results_list = []
-        #
-        # for result in results:
-        #     i, \
-        #     f_c_result,  = result.result()
-        #
-        #     data = {
-        #         'i': i,
-        #         'f_c_indi': f_c_result,
-        #     }
-        #     results_list.append(data)
-        #
-        # results_df = pd.DataFrame(results_list)
-        # results_dict = results_df.to_dict(orient='list')
-        # np.savez(folder_address + str(int(T_hat_try)) + "_indi.npz", **results_dict)
+    results_df = pd.DataFrame(results_list)
+    results_dict = results_df.to_dict(orient='list')
+    np.savez(folder_address + "benchmark_wel_flow.npz", **results_dict)
 
 
 if __name__ == '__main__':
